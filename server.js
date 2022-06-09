51ca83d694e6b51b7f88bbfaeb1a00961179ce02
Manoj Patidar <102366116+Manoj-Patidarrr@users.noreply.github.com>
2022-06-09 14:53:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use("/uploads", express.static("uploads"));
 app.use(cors({ credentials: true }));
 app.use("/shethink/v1", route(express.Router()));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Hello World!mano</h1>')
 })
 
 const port = process.env.PORT || 3211;
